4905a5c48eb666f104ae86072597ded2054b0f52
wheatwaves <SpaceOdyssey2077@gmail.com>
2022-09-11 12:57:57 -0700

    start

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
-}
+import "solmate/tokens/ERC20.sol";
+import "./libraries/Math.sol";
