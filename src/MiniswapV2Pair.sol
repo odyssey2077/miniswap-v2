87c8d63a3e9ca6303f8999a6ee691c48b70c54ed
wheatwaves <SpaceOdyssey2077@gmail.com>
2022-09-12 23:41:03 -0700

    add swap function

--- src/MiniswapV2Pair.sol
+++ src/MiniswapV2Pair.sol
@@ -12,6 +12,10 @@ interface IERC20 {
 error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
 error TransferFailed();
+error InsufficientOutputAmount();
+error InsufficientLiquidity();
+error InvalidK();
+error BalanceOverflow();
 
 contract MiniswapV2Pair is ERC20, Math {
     uint256 constant MINIMUM_LIQUIDITY = 1000;
@@ -20,21 +24,32 @@ contract MiniswapV2Pair is ERC20, Math {
     
     uint112 private reserve0;
     uint112 private reserve1;
+    uint32 private blockTimestampLast;
+
+    uint private unlocked = 1;
 
     event Burn(address indexed sender, uint256 amount0, uint256 amount1);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Sync(uint256 reserve0, uint256 reserve1);
+    event Swap(address indexed sender, uint256 amount0, uint256 amount1, address to);
 
+    modifier lock() {
+        require(unlocked == 1);
+        unlocked = 0;
+        _;
+        unlocked = 1;
+    }
     constructor(address _token0, address _token1) ERC20("MiniswapV2 LP", "MINIV2", 18) {
         token0 = _token0;
         token1 = _token1;
     }
 
     function mint() public {
+        (uint112 reserve0_, uint112 reserve1_,) = getReserves();
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
-        uint256 amount0 = balance0 - reserve0;
-        uint256 amount1 = balance1 - reserve1;
+        uint256 amount0 = balance0 - reserve0_;
+        uint256 amount1 = balance1 - reserve1_;
 
         uint256 liquidity;
 
@@ -50,11 +65,12 @@ contract MiniswapV2Pair is ERC20, Math {
 
         if (liquidity <= 0) revert InsufficientLiquidityMinted();
         _mint(msg.sender, liquidity);
-        _update(balance0, balance1);
+        _update(balance0, balance1, reserve0_, reserve1_);
         emit Mint(msg.sender, amount0, amount1);
     }
 
     function burn() public {
+        (uint112 reserve0_, uint112 reserve1_,) = getReserves();
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
         uint256 liquidity = balanceOf[msg.sender];
@@ -70,14 +86,43 @@ contract MiniswapV2Pair is ERC20, Math {
         balance0 = IERC20(token0).balanceOf(address(this));
         balance1 = IERC20(token1).balanceOf(address(this));
 
-        _update(balance0, balance1);
+        _update(balance0, balance1, reserve0_, reserve1_);
 
         emit Burn(msg.sender, amount0, amount1);
     }
 
-    function _update(uint256 balance0, uint256 balance1) private {
+    function swap(uint256 amount0Out, uint256 amount1Out, address to) public {
+        if (amount0Out == 0 && amount1Out == 0) {
+            revert InsufficientOutputAmount();
+        }
+        (uint112 reserve0_, uint112 reserve1_,) = getReserves();
+
+        if (amount0Out > reserve0_ || amount1Out > reserve1_) {
+            revert InsufficientLiquidity();
+        }
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        if (balance0 * balance1 < reserve0_ * reserve1_) 
+            revert InvalidK();
+        
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+    }
+
+    function _update(uint256 balance0, uint256 balance1, uint112 reserve0_, uint112 reserve1_) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max)
+            revert BalanceOverflow();
+
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
+
         emit Sync(reserve0, reserve1);
     }
 
