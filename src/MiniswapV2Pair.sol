4fb2ba4d95ad01aa017d7e9e445bea1af91adc74
wheatwaves <SpaceOdyssey2077@gmail.com>
2022-09-14 00:24:29 -0700

    add price oracle function

--- src/MiniswapV2Pair.sol
+++ src/MiniswapV2Pair.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.10;
 
 import "solmate/tokens/ERC20.sol";
 import "./libraries/Math.sol";
+import "./libraries/UQ112x112.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -18,6 +19,7 @@ error InvalidK();
 error BalanceOverflow();
 
 contract MiniswapV2Pair is ERC20, Math {
+    using UQ112x112 for uint224;
     uint256 constant MINIMUM_LIQUIDITY = 1000;
     address public token0;
     address public token1;
@@ -25,6 +27,10 @@ contract MiniswapV2Pair is ERC20, Math {
     uint112 private reserve0;
     uint112 private reserve1;
     uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+
 
     uint private unlocked = 1;
 
@@ -118,6 +124,14 @@ contract MiniswapV2Pair is ERC20, Math {
     function _update(uint256 balance0, uint256 balance1, uint112 reserve0_, uint112 reserve1_) private {
         if (balance0 > type(uint112).max || balance1 > type(uint112).max)
             revert BalanceOverflow();
+
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast += uint256(UQ112X112.encode(reserve1_).uqdiv(reserve0_)) * timeElapsed;
+                price1CumulativeLast += uint256(UQ112X112.encode(reserve0_).uqdiv(reserve1_)) * timeElapsed;
+            }
+        }
 
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
